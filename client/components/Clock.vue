3473f1759c494951e3446fbe320b79202ceeba15
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2024-09-24 19:42:35 +0200

    Update clock live

--- client/components/Clock.vue
+++ client/components/Clock.vue
@@ -1,54 +1,58 @@
 <template>
-  <span v-if='simData' class="text-2xl font-bold text-gray-800">
-    {{ secondsToTime(simData.clock.clock) }}
-  </span>
-  <span v-else class="text-2xl font-bold text-gray-800">
-    {{time}}
-  </span>
+  {{ time }}
 </template>
 
 <script>
-  export default {
-    name: "Clock",
-    props: ["simData"],
-    data() {
-      return {
-        interval: null,
-        time: null
-      }
-    },
-    beforeDestroy() {
-      // prevent memory leak
-      clearInterval(this.interval)
-    },
-    created() {
-      // update the time every second
-      this.interval = setInterval(() => {
+export default {
+  name: "Clock",
+  props: ["clockData"],
+  data () {
+    return {
+      interval: null,
+      time: null,
+      simTime: null
+    };
+  },
+  methods: {
+    calculateTime () {
+      const time = this.clockData;
+      if (time) {
+        const msPassed = Date.now() - time.lastReportedAt;
+        const secondsPassed = msPassed / 1000;
+        const timeNow = time.secondsSinceMidnight + (time.isPaused ? 0 : secondsPassed * time.speed);
+        const dateObj = new Date(0, 0, 0, 0, 0, timeNow);
+        const hours = dateObj.getUTCHours();
+        const minutes = dateObj.getUTCMinutes();
+        const seconds = dateObj.getSeconds();
+        this.time = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+      } else {
         // Concise way to format time according to system locale.
         // In my case this returns "3:48:00 am"
         this.time = Intl.DateTimeFormat(navigator.language, {
           hour: 'numeric',
           minute: 'numeric',
           second: 'numeric'
-        }).format()
-      }, 1000)
-    },
-    mounted() {
-      // this.socket.on("test", function (msg)
-      // {
-      //   console.log(msg);
-      // });
-      // this.socket.emit("extraTest", {nolo:false});
-    },
-    methods: {
-      secondsToTime(givenSeconds){
-        const dateObj = new Date(givenSeconds * 1000);
-        const hours = dateObj.getUTCHours();
-        const minutes = dateObj.getUTCMinutes();
-        const seconds = dateObj.getSeconds();
-        const timeString = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
-        return timeString;
+        }).format();
       }
     },
-  }
+    bindInterval () {
+      this.interval = setInterval(this.calculateTime, 1000 / this.clockData?.speed ?? 1);
+    },
+    unbindInterval () {
+      clearInterval(this.interval);
+    },
+  },
+  mounted () {
+    this.bindInterval();
+  },
+  watch: {
+    "clockData.speed": function () {
+      this.unbindInterval();
+      this.bindInterval();
+    }
+  },
+  beforeUnmount () {
+    this.unbindInterval();
+  },
+};
 </script>

66198d9a4372197e63d3ef1ec45c4df8502078b1
Charlotte Monroe <charlotte.monroe@theletc.co.uk>
2024-11-21 09:43:36 +0000

    Various improvements to the selector, including better contextual identification for sim and panel.
    Opens #4 - currently, the two columns do not have correct height/overflow behaviour

--- client/components/Selector.vue
+++ client/components/Selector.vue
@@ -1,35 +1,34 @@
 <template>
-  <div class="flex p-2">
+  <div class="flex pt-2">
     <div class="grid w-full p-2">
       <h1 class="text-center text-3xl font-bold">Claim Panels</h1>
     </div>
   </div>
-  <div class="flex p-2">
-    <div class="grid grid-cols-2 gap-2 w-full h-full">
+  <div class="flex p-2 h-4/5">
+    <div class="grid grid-cols-2 gap-2 w-full">
       <div class="w-full border-2 border-zinc-400">
-        <div class="">
-          <h2 class="text-center text-xl font-bold w-full border-b-2 border-zinc-400 overflow-scroll overscroll-contain ">Available Panels</h2>
+        <div class="w-full border-b-2 border-zinc-400">
+          <h2 class="text-center text-xl font-bold ">Available Panels</h2>
         </div>
-        <div class="bg-neutral-200">
-          <div class="text-center text-xl uppercase w-full">
-            <h3 class="border-b border-black p-2"><span class="text-zinc-800 text-base normal-case">Aston //</span> Alrewas</h3>
+        <div class="bg-neutral-200 overflow-scroll overscroll-contain">
+          <div class="text-center text-lg w-full">
             <template v-for="sim in gameData">
               <template v-for="panel in sim.panels">
-                <h3 v-if="!panel.player" class="border-b border-black p-2" @click="claimPanel(sim.id, panel.id)">{{ sim.name }} {{panel.name}}</h3>
+                <h3 v-if="!panel.player" class="border-b border-black p-2" @click="claimPanel(sim.id, panel.id)"><button class="font-mono underline border-2 border-green-800 p-1 rounded decoration-dotted text-sm">{{sim.name}}</button> {{panel.name}}</h3>
               </template>
             </template>
           </div>
         </div>
       </div>
       <div class="w-full border-2 border-zinc-400">
-        <div class="">
-          <h2 class="text-center text-xl font-bold w-full border-b-2 border-zinc-400">Claimed Panels</h2>
+        <div class="w-full border-b-2 border-zinc-400">
+          <h2 class="text-center text-xl font-bold">Claimed Panels</h2>
         </div>
-        <div class="bg-neutral-200">
+        <div class="bg-neutral-200 overflow-scroll overscroll-contain h-99">
           <div class="text-center text-lg w-full">
             <template v-for="sim in gameData">
               <template v-for="panel in sim.panels">
-                <h3 v-if="panel.player === username" class="border-b border-black p-2" @click="releasePanel(sim.id, panel.id)">{{ sim.name }} {{panel.name}}</h3>
+                <h3 v-if="panel.player === username" class="border-b border-black p-2" @click="releasePanel(sim.id, panel.id)"><button class="font-mono underline border-2 border-green-800 p-1 rounded decoration-dotted text-sm">{{sim.name}}</button> {{panel.name}}</h3>
               </template>
             </template>
           </div>

921c0519304cbadfdd30a652ea84d72402f97efd
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2024-12-28 16:09:26 +0100

    Move to WelcomePage component

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,39 +1,26 @@
 <template>
-    <div class="container max-w-full h-screen pt-4 bg-slate-900 text-lime-500 font-mono">
-      <div class="text-right text-xs">
-        <p>host: <b>april</b></p>
-        <p>client: <b>2.0.0</b></p>
-        <p>server: <b>2.0.0</b></p>
-      </div>
-      <Logo />
-      <p class="text-3xl text-center">
-        Railway Operations Centre
-      </p>
-      <p class="mt-10 text-center text-xl">
-        Restricted Access - Authentication Required: <span class="relative w-[max-content] font-mono
-before:absolute before:inset-0 before:bg-slate-900 before:animate-typewriter">....</span>
-      </p>
-      <p class="text-center text-xl m-5">
-        <button class="rounded-xl bg-lime-500 text-slate-900 p-3 hover:bg-lime-700 hover:text-slate-50 hover:fill-slate-50" @click="signIn('discord')"><DiscordLogo class="inline mx-1 " width="40"/> Login with Discord</button>
-      </p>
-      <p class="text-center text-sm m-50">
-        <a href="/admin" class="hover:underline">Admin Console</a> &cir;
-        <a href="https://simsig.co.uk" target="_blank" class="hover:underline">SimSig</a> &cir;
-        <a href="https://bradshaw.onourlines.co.uk/wiki/SimSig:ROC" target="_blank" class="hover:underline">User Guide</a>
-      </p>
-      <div class="flex pt-5 pb-3 items-center">
-        <div class="flex-grow border-t border-2 border-dashed border-lime-700"></div>
-      </div>
-      <GameStatus/>
-    </div>
+	<WelcomePage>
+		<p class="mt-10 text-center text-xl">
+			Restricted Access - Authentication Required:
+			<span
+				class="relative w-[max-content] font-mono before:absolute before:inset-0 before:bg-slate-900 before:animate-typewriter">....</span>
+		</p>
+		<p class="text-center text-xl m-5">
+			<button
+				class="rounded-xl bg-lime-500 text-slate-900 p-3 hover:bg-lime-700 hover:text-slate-50 hover:fill-slate-50"
+				@click="signIn('discord')">
+				<DiscordLogo class="inline mx-1 " width="40" /> Login with Discord
+			</button>
+		</p>
+	</WelcomePage>
 </template>
 
 <script setup lang="ts">
-  definePageMeta({
-      auth: {
-          unauthenticatedOnly: true,
-          navigateAuthenticatedTo: '/',
-      }
-  })
-  const { signIn } = useAuth()
+definePageMeta({
+	auth: {
+		unauthenticatedOnly: true,
+		navigateAuthenticatedTo: '/',
+	}
+});
+const { signIn } = useAuth();
 </script>
--- client/pages/status.vue
+++ client/pages/status.vue
@@ -1,16 +1,11 @@
 <template>
-	<div class="container max-w-full h-screen pt-4 bg-slate-900 text-lime-500 font-mono">
-		<Logo />
-		<p class="text-3xl text-center">
-			Railway Operations Centre
-		</p>
+	<WelcomePage>
 		<p class="text-center text-xl m-5">
 			<a href="/"
 				class="rounded-xl bg-lime-500 text-slate-900 p-3 hover:bg-lime-700 hover:text-slate-50 hover:fill-slate-50">
 				Load ROC</a>
 		</p>
-		<GameStatus />
-	</div>
+	</WelcomePage>
 </template>
 
 <script setup lang="ts">

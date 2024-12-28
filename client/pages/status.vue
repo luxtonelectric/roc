ea35e25e67d4f99da764c28e20572bc68036a6d8
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2024-12-28 15:50:53 +0100

    Add separate status page

---
+++ client/pages/status.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="container max-w-full h-screen pt-4 bg-slate-900 text-lime-500 font-mono">
+		<Logo />
+		<p class="text-3xl text-center">
+			Railway Operations Centre
+		</p>
+		<p class="text-center text-xl m-5">
+			<a href="/"
+				class="rounded-xl bg-lime-500 text-slate-900 p-3 hover:bg-lime-700 hover:text-slate-50 hover:fill-slate-50">
+				Load ROC</a>
+		</p>
+		<GameStatus />
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
